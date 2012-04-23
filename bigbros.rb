e37a7180746dbca0a1a2dce1f67a64fc87de443c
Rahul <rahul@rahulswaminathan.com>
2012-04-22 22:30:40 -0700

    erything in ruby

--- bigbros.rb
+++ bigbros.rb
@@ -5,28 +5,31 @@ require 'gmail'
 require 'base64'
 require 'yaml'
 
-exit if rand(20) < 19
+while true do
 
-config = YAML.load_file(File.expand_path("gmail.yml"))
+  config = YAML.load_file(File.expand_path("gmail.yml"))
 
-t = Time.now.strftime("%d-%m-%y-%H-%M-%S")
-`screencapture ./#{t}.png`
+  t = Time.now.strftime("%d-%m-%y-%H-%M-%S")
+  `screencapture ./#{t}.png`
 
-msg = "Random screenshot"
+  msg = "Random screenshot"
 
 
-gmail = Gmail.new(config["email"], Base64.decode64(config["password"]))
+  gmail = Gmail.new(config["email"], Base64.decode64(config["password"]))
 
-gmail.deliver do 
-  to config["to"]
-  subject "Random screenshot"
-  text_part do
-    "check this out"
+  gmail.deliver do 
+    to config["to"]
+    subject "Random screenshot"
+    text_part do
+      "check this out"
+    end
+    add_file "./#{t}.png"
   end
-  add_file "./#{t}.png"
+
+  gmail.logout
+
+  #remove file
+  `rm #{t}.png`
+
+  sleep(60*rand(20)+60)
 end
-
-gmail.logout
-
-#remove file
-`rm #{t}.png`
